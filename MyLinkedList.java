a752caaba528207f66aafc17af6b74c9502d7782
CodyBagwandeen <cbagwandeen@stuy.edu>
2019-03-29 10:06:39 -0400

    put Node in MyLinkedList

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -167,4 +167,47 @@ public class MyLinkedList{
       other.end = null;
       other.size = 0;
     }
+
+    public class Node{
+ private Integer data;
+ private Node next,prev;
+
+public Node( Integer val, Node Next, Node previous) {
+  data = val;
+  next = Next;
+  prev = previous;
+}
+
+ public int getData() {
+   return data;
+ }
+
+ public Node next() {
+   return next;
+ }
+
+ public void setNext( Node val) {
+   next = val;
+ }
+
+ public Node prev() {
+   return prev;
+ }
+
+ public void setPrev( Node val) {
+   prev = val;
+ }
+
+ public Integer setData(Integer i) {
+   Integer temp = data;
+   data = i;
+   return temp;
+ }
+
+ public String toString() {
+   return "" + data;
+ }
+
+}
+
 }
